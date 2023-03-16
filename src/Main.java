9c8c0ba7642a5d0c6fb3dcd318c391dbc50cd1ad
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-16 17:52:15 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import java.util.Calendar;
+
 public class Main {
+    static String date() {
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        int month = 1 + Calendar.getInstance().get(Calendar.MONTH);
+        int day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+
+        return String.format("%d-%02d-%02d",year, month, day);
+    }
+
     public static void main(String[] args) {
         // Skapa och anropa en metod som returnerar dagens datum
         // på formatet yyyy-mm-dd
 
-
+        System.out.println("Dagens datum: " + date());
     }
 }
